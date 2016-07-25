4ba8ee38d8cea77f021099b464713c8a0696e21c
Jérôme Carretero <cJ@zougloub.eu>
2016-07-25 02:47:14 -0400

    fixup py3 build

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+import io, os, tempfile, subprocess, shutil
+
 from distutils.core import setup
 from Cython.Distutils import Extension
 from Cython.Distutils import build_ext
+
 import numpy
 
-import os, tempfile, subprocess, shutil
 
 # see http://openmp.org/wp/openmp-compilers/
-omp_test = r"""#include <omp.h>
+omp_test = br"""#include <omp.h>
 #include <stdio.h>
 int main() {
 #pragma omp parallel
@@ -16,21 +18,18 @@ printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_
 
 def has_openmp():
     tmpdir = tempfile.mkdtemp()
-    curdir = os.getcwd()
-    os.chdir(tmpdir)
 
-    filename = r'test.c'
-    file = open(filename,'w', 0)
-    file.write(omp_test)
-    with open(os.devnull, 'w') as fnull:
-        result = subprocess.call(['cc', '-fopenmp', filename], stdout=fnull,
-                                 stderr=fnull)
+    filename = os.path.join(tmpdir, 'test.c')
+    with io.open(filename, 'wb') as f:
+        f.write(omp_test)
+    with io.open(os.devnull, 'wb') as fnull:
+        result = subprocess.call(['cc', '-fopenmp', filename],
+         stdout=fnull,
+         stderr=fnull,
+         cwd=tmpdir,
+        )
 
-    file.close
-    os.chdir(curdir)
-    #clean up
     shutil.rmtree(tmpdir)
-
     return result == 0
 
 extra_compile_args = ['-O3']
